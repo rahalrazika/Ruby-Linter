4452e970f3a5915b129f7a4a378c50d8954547c2
jacobrees <jacob.rees97@yahoo.com>
2020-08-07 19:18:18 +0100

    Create read_file method that prints opening file and displays amount of lines that are within the file to be checked

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,15 @@
 #!/usr/bin/env ruby
+
+class Linter
+  def read_file(file)
+    puts 'Opening file...'
+    puts "Total lines of code within file to be assesed: #{File.open(file).readlines.size}"
+  end
+
+  def run
+    read_file('bin/error/test.rb')
+  end
+end
+
+lint = Linter.new
+lint.run
