fb8ebc93b3b277b33baf43ecb92357c86abc7eb7
jacobrees <jacob.rees97@yahoo.com>
2020-08-07 20:58:00 +0100

    Add condition that code will exit if specified path doesn't end with .rb, linter will only accept Ruby files

--- bin/main.rb
+++ bin/main.rb
@@ -3,13 +3,18 @@
 class Linter
   def read_file(file)
     puts 'Launching DragonLint...'
-    puts 'Opening file...'
-    begin
-      puts "Total lines of code to be checked within '#{file}': #{File.open(file).readlines.size}"
-      puts 'Searching for linting errors...'
-    rescue StandardError
-      puts "Unable to open file '#{file}' please ensure specified path given is correct."
+    puts 'Opening .rb file...'
+    if file[-2, 2] != 'rb'
+      puts "Ruby file not found. The specified path '#{file}' does not end with .rb"
       exit
+    else
+      begin
+        puts "Total lines of code to be checked within '#{file}': #{File.open(file).readlines.size}"
+        puts 'Searching for linting errors...'
+      rescue StandardError
+        puts "Unable to open file '#{file}' please ensure specified path given is correct."
+        exit
+      end
     end
   end
 
