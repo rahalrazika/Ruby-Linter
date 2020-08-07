37a5385717442e5e3084af1784fc2995e134058f
jacobrees <jacob.rees97@yahoo.com>
2020-08-07 18:59:59 +0100

    Create test cases for naming variables, classes, modules and methods

---
+++ bin/error/test.rb
@@ -0,0 +1,35 @@
+#good variable name
+example_var = 0
+
+#bad variable name
+ExampleVar = 0
+
+#good method name
+def example_method
+    #code
+end
+
+#bad method name
+def ExampleMethod
+    #code
+end
+
+#good class name
+class ExampleClass
+    #code
+end
+
+#bad class name
+class example_class
+    #code
+end
+
+#good module name
+module ExampleModule
+    #code
+end
+
+#bad module name
+module example_module
+    #code
+end
