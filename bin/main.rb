1686f6f229eeef9aa7e93d8a75ba76d4eb281a32
jacobrees <jacob.rees97@yahoo.com>
2020-08-10 12:10:44 +0100

    Disable Metrics/AbcSize cop for display_results method, add colors to console output

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require_relative '../lib/naming_test.rb'
+require_relative '../lib/string_colors.rb'
 
 class Linter
   include NamingTest
@@ -12,7 +13,7 @@ class Linter
   end
 
   def launch
-    puts 'Launching DragonLintAlpha...'
+    puts 'Launching ' + 'DragonLintAlpha...'.red
     puts 'Please enter which Ruby file from bin/error/ directory you would like to scan.'
     puts 'Enter "*" to scan all .rb files.'
     @file = gets.chomp
@@ -20,16 +21,16 @@ class Linter
   end
 
   def read_file(file)
-    puts 'Opening .rb file...'
+    puts 'Opening .rb file...'.yellow
     if file[-3, 3] != '.rb'
-      puts "Ruby file not found. The specified path '#{file}' does not end with .rb"
+      puts "Ruby file not found. The specified path '#{file}' does not end with .rb".red
       exit
     else
       begin
-        puts "Total lines of code to be checked within '#{file}': #{File.open(file).readlines.size}"
-        puts 'Searching for linting errors...'
+        puts "Total lines of code to be checked within '#{file}': #{File.open(file).readlines.size}".yellow
+        puts 'Searching for linting errors...'.yellow
       rescue StandardError
-        puts "Unable to open file '#{file}' please ensure specified path given is correct."
+        puts "Unable to open file '#{file}' please ensure specified path given is correct.".red
         exit
       end
     end
@@ -37,35 +38,37 @@ class Linter
   end
 
   # rubocop: disable Metrics/MethodLength
+  # rubocop: disable Metrics/AbcSize
   def display_results(file)
-    puts " "
+    puts ' '
     puts '---------'
-    puts "Errors inside of #{file}:"
+    puts "Errors inside of #{file}:".cyan
     puts '---------'
     test_variable_name(file).each do |key, code|
-      puts "  |#{file}| Error: Syntax/VariableName(use_snake_case)"
-      puts '    Line: ' + key.to_s + ": #{code}"
+      puts "  |#{file}| Error: Syntax/VariableName(use_snake_case)".yellow
+      puts '    Line: '.cyan + key.to_s.cyan + ':'.cyan + " #{code}".red
       puts '  ---------'
     end
     test_method_name(file).each do |key, code|
-      puts "  |#{file}| Error: Syntax/MethodName(use_snake_case)"
-      puts '    Line: ' + key.to_s + ": #{code}"
+      puts "  |#{file}| Error: Syntax/MethodName(use_snake_case)".yellow
+      puts '    Line: '.cyan + key.to_s.cyan + ':'.cyan + " #{code}".red
       puts '  ---------'
     end
     test_class_name(file).each do |key, code|
-      puts "  |#{file}| Error: Syntax/ClassName(UsePascalCase)"
-      puts '    Line: ' + key.to_s + ": #{code}"
+      puts "  |#{file}| Error: Syntax/ClassName(UsePascalCase)".yellow
+      puts '    Line: '.cyan + key.to_s.cyan + ':'.cyan + " #{code}".red
       puts '  ---------'
     end
     test_module_name(file).each do |key, code|
-      puts "  |#{file}| Error: Syntax/ModuleName(UsePascalCase)"
-      puts '    Line: ' + key.to_s + ": #{code}"
+      puts "  |#{file}| Error: Syntax/ModuleName(UsePascalCase)".yellow
+      puts '    Line: '.cyan + key.to_s.cyan + ':'.cyan + " #{code}".red
       puts '  ---------'
     end
     puts '---------'
-    puts " "
+    puts ' '
   end
   # rubocop: enable Metrics/MethodLength
+  # rubocop: enable Metrics/AbcSize
 
   def read_all
     file_names = Dir['./bin/error/*.rb'].map { |path| path[12, path.size] }
