06e57acea1ba5c2336ad6e1bdae326d5c126c954
jacobrees <jacob.rees97@yahoo.com>
2020-08-10 14:37:47 +0100

    Change chop to strip in space_test.rb line:10

--- lib/space_test.rb
+++ lib/space_test.rb
@@ -7,7 +7,7 @@ module SpaceTest
     line_number = 0
     File.open(file, 'r').each_line do |line|
       line_number += 1
-      @each_trailing_space_error[line_number] = line.strip.chop if test_if_trailing_space(line) == true
+      @each_trailing_space_error[line_number] = line.strip if test_if_trailing_space(line) == true
     end
     @each_trailing_space_error
   end
